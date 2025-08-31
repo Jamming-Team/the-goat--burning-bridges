cd4bf25e313398429fe549ec53cde31dbb905739
Xidriel <stormingisle@proton.me>
2025-08-31 04:37:01 +0300

    Trying to do some graphics

--- Scripts/Player/blood_strains_component.gd
+++ Scripts/Player/blood_strains_component.gd
@@ -18,10 +18,11 @@ func _spawn_stain():
 
 	#print_debug(_raycast.get_collision_point())
 
-	var decal: Decal = _decal_scene.instantiate() as Decal
-	decal.texture_albedo = _blood_stain_sprites[randi() % _blood_stain_sprites.size()]
+	var decal: DecalCompatibility = _decal_scene.instantiate() as DecalCompatibility
+	decal.texture = _blood_stain_sprites[randi() % _blood_stain_sprites.size()]
 	decal.rotate_y(randi() % 360)
 	var collider_node = _raycast.get_collider() as Node
 	collider_node.add_child(decal)
 	decal.global_position = _raycast.get_collision_point()
+	decal.global_position.y -= 0.3
 	
